f5aae1ef766e8557b3f9bd0674dd3fccaf08bf88
figericnewton <fig.eric.newton@gmail.com>
2020-05-08 00:33:26 -0400

    copy data directory whole

--- tools/deploy.py
+++ tools/deploy.py
@@ -7,20 +7,12 @@ webDir = os.path.join('..', 'web')
 target = 'D:' #configure to match SD card drive
 
 #remove the old files from SD card
-for dir in os.listdir(dataDir):
-  toDelete = os.path.join(target, dir)
+for toDelete in [os.path.join(target, 'web'), os.path.join(target, 'data')]:
   if os.path.exists(toDelete):
     shutil.rmtree(toDelete)
-
-toDelete = os.path.join(target, 'web')
-if os.path.exists(toDelete):
-  shutil.rmtree(toDelete)
 
 time.sleep(1)
 
 #copy over the new files to the SD card
-for dir in os.listdir(dataDir):
-  if os.path.isdir(dir):
-    shutil.copytree(os.path.join(dataDir, dir), os.path.join(target, dir))
-
+shutil.copytree(dataDir, os.path.join(target, 'data'))
 shutil.copytree(webDir, os.path.join(target, 'web'))
