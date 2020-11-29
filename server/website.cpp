167f6678b103dcdc9a4cd9ddcfc41ab0fb7788a9
figericnewton <fig.eric.newton@gmail.com>
2020-11-28 22:42:03 -0500

    add license header
    Added a GPL3 license header to the source files. Not sure what's best to add to the HTML stuff though since I don't necessarily want to bog down the microcontroller with sending extra bytes.

--- server/website.cpp
+++ server/website.cpp
@@ -1,3 +1,20 @@
+/*****************************************************************************
+* LEDimageServer - webserver and controller application for a screen of LEDs *
+* Copyright (C) 2020 Eric Newton                                             *
+*                                                                            *
+* LEDimageServer is free software: you can redistribute it and/or modify     *
+* it under the terms of the GNU General Public License as published by       *
+* the Free Software Foundation, either version 3 of the License, or          *
+* (at your option) any later version.                                        *
+*                                                                            *
+* LEDimageServer is distributed in the hope that it will be useful,          *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+* GNU General Public License for more details.                               *
+*                                                                            *
+* You should have received a copy of the GNU General Public License          *
+* along with LEDimageServer.  If not, see <https://www.gnu.org/licenses/>.   *
+*****************************************************************************/
 
 #ifndef WEBSITE_CPP
 #define WEBSITE_CPP
